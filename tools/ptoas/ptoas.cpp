7bf010892f96d4ec5e1dda9ea3070311f507bc6c
mouliangyu <21963576+mouliangyu@users.noreply.github.com>
2026-07-01 12:33:31 +0800

    Optimize VMI group broadcast load layout

--- tools/ptoas/ptoas.cpp
+++ tools/ptoas/ptoas.cpp
@@ -1739,6 +1739,12 @@ static LogicalResult runVMISemanticPipeline(OwningOpRef<ModuleOp> &module) {
   PassManager pm(module->getContext());
   pm.enableVerifier();
   pm.addPass(pto::createPTOValidateVMIIRPass());
+  pm.addPass(createCanonicalizerPass());
+  pm.addPass(createCSEPass());
+  pm.addPass(pto::createVMIPreAssignmentCombinePass());
+  pm.addPass(createCanonicalizerPass());
+  pm.addPass(createCSEPass());
+  pm.addPass(pto::createVMILegalizeArithSelectPass());
   pm.addPass(pto::createVMILayoutAssignmentPass());
   pm.addPass(createCanonicalizerPass());
   pm.addPass(createCSEPass());
